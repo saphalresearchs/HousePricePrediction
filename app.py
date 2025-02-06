e3ebdcf1d9db4429cb916f3e62a505fc6c2de0bb
Saphalya Acharya <programmersaphal@gmail.com>
2025-02-06 23:08:11 +0545

    Model fixing needs to be done

---
+++ app.py
@@ -0,0 +1,35 @@
+import streamlit as st
+import pickle
+import numpy as np
+# from xgboost import XGBRegressor
+
+with open("model.pkl","rb") as file:
+    pickle.load(file)
+
+model = pickle.load(open("model.pkl", "rb"))
+
+
+st.title("Boston House Price Predictinn")
+
+st.write("Enter the required details to predict house prices: ")
+
+#inputfields
+CRIM = st.number_input("CRIM", min_value=0, max_value=100, step=0.002)	
+ZN = st.number_input("ZN", min_value=0, max_value=100, step=1) 
+INDUS = st.number_input("INDUS", min_value=0, max_value=100, step=0.5)	
+CHAS = st.number_input("CHAS", min_value=0, max_value=1, step=0.01)	
+NOX = st.number_input("NOX", min_value=0, max_value=1, step=0.01)	
+RM = st.number_input("RM", min_value=0, max_value=9, step=0.01)	
+AGE = st.number_input("AGE", min_value=0, max_value=150, step=0.1)	
+DIS = st.number_input("DIS", min_value=0, max_value=50, step=0.01)	
+RAD = st.number_input("RAD", min_value=1, max_value=24, step=1)	
+TAX = st.number_input("TAX", min_value=0, max_value=10000, step=1)	
+PTRATIO = st.number_input("PTRATIO", min_value=0, max_value=100, step=0.1)	
+B = st.number_input("B", max_value=0, min_value=1000, step=1)	
+LSTAT = st.number_input("LSTAT", min_value=0, max_value=100, step=0.1)
+
+if st.button("Predict"):
+    input_data = np.array[CRIM,ZN,INDUS,CHAS,NOX,RM,AGE,DIS,RAD,TAX,PTRATIO,B,LSTAT]
+    input_data_reshaped = np.reshape(1,-1)
+    prediction = model.predict(input_data_reshaped)
+    st.success("The price of house is: ", prediction[0])
